4629a2f1f2a6d026545d3e576ee89e94791b9794
osboxes.org <osboxes@osboxes.org>
2021-02-14 10:38:25 -0500

    add s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
+    key    = "terraform/terraform.tfstate"
+    # region = "eu-west-1"
   }
-}
