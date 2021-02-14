29a5a6ec8080092c875181a5bdd3f44dc06fe807
osboxes.org <osboxes@osboxes.org>
2021-02-14 08:41:22 -0500

    change ciderblock

--- terraform/1-vpc.tf
+++ terraform/1-vpc.tf
@@ -3,7 +3,7 @@
 ###################
 
 resource "aws_vpc" "new" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "20.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
