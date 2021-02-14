311bfd6513611574c2155e7aa0e11d8d2f07fbb9
osboxes.org <osboxes@osboxes.org>
2021-02-14 12:15:11 -0500

    add s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,17 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state"
-    key    = "terraform/terraform.tfstate"
-    # region = "eu-west-1"
+
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "test"
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "mysamar1234-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
