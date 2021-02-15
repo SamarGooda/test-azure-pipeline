20c0174b0574bc4390c2680de265999d708b8f82
osboxes.org <osboxes@osboxes.org>
2021-02-15 08:17:13 -0500

    add s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,31 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "mama-tf-test-bucket"
+resource "aws_s3_bucket" "taas-tt-dev-bucket" {
+  bucket = "taas-tt-dev-bucket"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "taas-tt-dev-bucket"
   }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "taas-tt-dev-bucket"
+    key            = "terraform.tfstate"
+    region         =  "eu-west-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+    profile = "test"
+  }
+
 }
