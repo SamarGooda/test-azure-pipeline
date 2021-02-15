94a23dbf12c469a9062c082d64ccabb98c0aecd0
osboxes.org <osboxes@osboxes.org>
2021-02-15 08:19:17 -0500

    add s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,26 +6,26 @@ resource "aws_s3_bucket" "taas-tt-dev-bucket" {
   }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform-up-and-running-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
 
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "taas-tt-dev-bucket"
-    key            = "terraform.tfstate"
-    region         =  "eu-west-1"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-    profile = "test"
-  }
+# terraform {
+#   backend "s3" {
+#     # Replace this with your bucket name!
+#     bucket         = "taas-tt-dev-bucket"
+#     key            = "terraform.tfstate"
+#     region         =  "eu-west-1"
+#     # Replace this with your DynamoDB table name!
+#     dynamodb_table = "terraform-up-and-running-locks"
+#     encrypt        = true
+#     profile = "test"
+#   }
 
-}
+# }
