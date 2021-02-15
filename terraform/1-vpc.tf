d8b412cdc76a16ed6fda22b60f72962f36af83b5
osboxes.org <osboxes@osboxes.org>
2021-02-15 09:19:49 -0500

    edit

--- terraform/1-vpc.tf
+++ terraform/1-vpc.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "new" {
 
 
 resource "aws_vpc" "main" {
-  cidr_block           = "30.0.0.0/16"
+  cidr_block           = "40.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
